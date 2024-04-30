54355d33d124009c151977889005177272f53a8c
James Kua <kua.james30@gmail.com>
2024-04-30 21:48:39 +0800

    Update constant

--- src/lib/_modules/constants.ts
+++ src/lib/_modules/constants.ts
@@ -3,4 +3,4 @@ export const TRANSACTIONS_INITIAL_MAX_SHOW = 5;
 export const TRANSACTIONS_INC_MAX_SHOW = 20;
 export const STORAGE_PREFIX = 'splitio';
 export const RECENT_GROUPS_KEY = `${STORAGE_PREFIX}_recent_groups`;
-export const MAX_RECENT_GROUPS_AMNT = 5;
+export const MAX_RECENT_GROUPS_AMNT = 10;
