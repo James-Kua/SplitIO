1712f5a39f1c2f692a935d283a0022942652b6be
James Kua <kua.james30@gmail.com>
2024-05-03 23:44:02 +0800

    Update currencies

--- src/lib/_modules/constants.ts
+++ src/lib/_modules/constants.ts
@@ -6,7 +6,13 @@ export const RECENT_GROUPS_KEY = `${STORAGE_PREFIX}_recent_groups`;
 export const MAX_RECENT_GROUPS_AMNT = 10;
 
 export const CURRENCY_SYMBOLS = {
-	SGD: '$',
-	RMB: '¥',
-	YEN: '¥'
+	SGD: '$', 
+	CNY: '¥', 
+	JPY: '¥',
+	KRW: '₩', 
+	THB: '฿',
+	PHP: '₱',
+	IDR: 'Rp',
+	MYR: 'RM',
+	VND: '₫', 
 };
